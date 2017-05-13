e46aa4659efd8f6f984d706483ce7a5a7610d128
Yogesh <metal4yogesh@gmail.com>
2017-05-13 15:16:59 +0545

    Optimizing code

--- 11.php
+++ 11.php
@@ -2,22 +2,26 @@
 //we have formula for triangle number is Tn = (n(n + 1))/2
 $divisorCount = 500;
 //so its no point searching for triangle number before 500th term
-$i = 0; $loop = true;
+$i = 10000; $loop = true; $maxDiv = 0;
 echo "\nWorking...\n";
 while($loop){
-
 //get the triangle term
 $triNum = ($i * ($i + 1) )/ 2;
 $divisors = array(1);
-$largestDivisor = (int)($triNum / 2);
+$largestDivisor = (int)sqrt($triNum);
 
 	for($j = 2; $j < $largestDivisor; $j++){
-		if($triNum % $j == 0 && !in_array($triNum/$j,$divisors)){
+		if($triNum % $j == 0){
 			$divisors[] = $j;
 			$divisors[] = $triNum / $j;
 		}
 	}
-	if(count($divisors) >= $divisorCount){
+	$nums = count($divisors);
+	if($nums > $maxDiv){
+		$maxDiv = $nums;
+		echo "\nReached $i term ($triNum) with ".$nums." divisors. \n";
+	}
+	if($nums >= $divisorCount){
 		$loop = false;
 		echo "\nThe first triangle number to have over $divisorCount divisors is $triNum. \n";
 	}
